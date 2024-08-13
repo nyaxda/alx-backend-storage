30a15c4ecf90965c9bd7658fe9cc66c5eb921e9b
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-14 02:36:04 +0300

    feat: Add count of schools by topic function

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,3 +5,4 @@
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
     count = mongo_collection.count_documents({"topics": topic})
+    return count
