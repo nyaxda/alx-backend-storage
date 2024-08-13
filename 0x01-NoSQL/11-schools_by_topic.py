0726fe3913cb4e5905f0efdf83db0e813fcbbf87
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-14 02:31:28 +0300

    Added File 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module for mongodb"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function that returns the list of school having a specific topic"""
+    count = mongo_collection.count_documents({"topics": topic})
