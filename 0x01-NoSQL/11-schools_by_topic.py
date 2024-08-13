f964fe3d9d2881b7a8b841d87b6702195d5dbe3f
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-14 02:37:05 +0300

    refactor: Update schools_by_topic function to use find instead of count_documents

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
-    count = mongo_collection.count_documents({"topics": topic})
+    count = mongo_collection.find({"topics": topic})
     return count
