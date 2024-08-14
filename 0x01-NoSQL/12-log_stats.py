56cefdf4724dbdaa48c74bf6efb4b0e6f67c350c
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-14 03:06:48 +0300

    Added Files 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""module for mongodb"""
+
+from pymongo import MongoClient
+
+
+def print_nginx_logs(dump):
+    """function that returns the list of school having a specific topic"""
+    print(f"{dump.estimated_document_count()} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {dump.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {dump.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {dump.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {dump.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {dump.count_documents({'method': 'DELETE'})}")
+    print(f"{dump.count_documents({
+        'method': 'GET', 'path': '/status'})} status check")
+
+
+def main():
+    """main function"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_logs(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    main()
