3e39777df25699235885a9de5d6d63c33a3fa246
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-15 00:36:53 +0300

    refactor: Add get, get_str, and get_int methods to Cache class for retrieving data from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable, Any
 
 
 class Cache:
@@ -14,9 +14,26 @@ class Cache:
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Hashing method to store data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str, fn: Optional[Callable] = None) -> Any:
+        """Get Method from redis"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Coverts bytes to str"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Get an int from redis"""
+        return self.get(key, fn=int)
