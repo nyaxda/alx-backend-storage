eb1494628c9501884109f2e45b49e1d3be4b2cf3
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-14 03:32:12 +0300

    refactor: Update print_nginx_logs function to use count_documents for log count

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+"""module for mongodb"""
+
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
+def print_nginx_logs(dump):
+    """function that returns the list of school having a specific topic"""
+    print(f"{dump.count_documents({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {dump.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {dump.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {dump.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {dump.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {dump.count_documents({'method': 'DELETE'})}")
+    status_check_count = dump.count_documents(
+        {'method': 'GET', 'path': '/status'}
+    )
+    print(f"{status_check_count} status check")
 
 
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+def main():
+    """main function"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    print_nginx_logs(client.logs.nginx)
 
 
 if __name__ == '__main__':
-    run()
+    main()
